c25ad3eeb1cab3f73daf748646c2878ded8d0164
Florian Gächter <florian.gaechter@gmail.com>
2020-05-28 16:58:25 +0200

    fix: change path to json

--- api/src/services/country/country.js
+++ api/src/services/country/country.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-import CountryInfo from './countryInfo.json'
+import CountryInfo from 'src/services/country/countryInfo.json'
 
 export const getCountry = async ({ id }) => {
   const countryCode = id.match(/^[a-z]{2}$/i)
